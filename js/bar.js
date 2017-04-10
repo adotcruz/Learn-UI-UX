c47a3803283710ef91c65ac0a3bd5e903ad28759
Anthony Cruz <ACruz@Anthonys-MacBook-Air.local>
2017-04-10 00:05:37 -0400

    almost done with index.html

--- js/bar.js
+++ js/bar.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
 	var $window = $(window),
 		$navbar = $('#navbar-data'),
-		top = $navbar.offset().top;
+		top = $navbar.offset().top,
+		xstop = $('#xs-nav').offset().top;
 
 	$window.scroll(function() {
-		$navbar.toggleClass('sticky', $window.scrollTop() > top + 65);
+		$navbar.toggleClass('sticky', $window.scrollTop() > top + 5);
+		// $('#xs-nav').toggleClass('sticky', $window.scrollTop() > xstop + 65);
 	});
 });
