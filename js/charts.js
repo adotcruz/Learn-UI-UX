1365ae81b774e5e189137d20bb89bc3270234eae
anthony cruz <ACruz@Anthonys-MacBook-Air.local>
2017-04-16 22:35:45 -0400

    more changes to the site

---
+++ js/charts.js
@@ -0,0 +1,40 @@
+var config = {
+  type: 'bar',
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [{
+      type: 'line',
+      label: 'Dataset 2',
+      data: [65, 10, 80, 81, 56, 85, 40],
+      borderColor: 'black',
+      fill: false
+    }, {
+      type: 'bar',
+      label: 'Dataset 1',
+      backgroundColor: "red",
+      data: [65, 0, 80, 81, 56, 85, 40],
+    }, {
+      type: 'bar',
+      label: 'Dataset 3',
+      backgroundColor: "blue",
+      data: [65, 0, 80, 81, 56, 85, 40]
+    }]
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        stacked: true
+      }],
+      yAxes: [{
+        stacked: true
+      }]
+    },
+    plugins: {
+      stacked100: { enable: true }
+    }
+  }
+};
+$(document).ready(function(){
+  var ctx = $("#chart-one")[0].getContext("2d");
+  new Chart(ctx, config);
+});
